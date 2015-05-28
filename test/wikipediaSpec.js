88617156f0f914e72f94f78c4fa2401bba487722
Jeremy Sklarsky <jeremy.sklarsky@gmail.com>
2015-05-28 17:24:27 -0400

    add comments to wiki test spec

--- test/wikipediaSpec.js
+++ test/wikipediaSpec.js
@@ -15,7 +15,9 @@ test.describe('Wikipedia', function() {
 
   test.it('should work', function() {
     this.timeout(4000);
+    // navigatge to page
     driver.get('http://www.wikipedia.com');
+    // check if we got there
     driver.getTitle().then(function(title) {
       title.should.equal("Wikipedia");
     });
@@ -23,10 +25,13 @@ test.describe('Wikipedia', function() {
 
   test.it('should be able to search for an article', function() {
     this.timeout(8000);
+    // navigate to home page
     driver.get('http://www.wikipedia.com');
-
+    // find search bar and type in input
     driver.findElement(webdriver.By.name("search")).sendKeys("javascript");
+    // click submit
     driver.findElement(webdriver.By.name("go")).click();
+    // check if we got sent where we wanted to be sent
     driver.getTitle().then(function(title){
       title.should.include("JavaScript");
     });
@@ -34,18 +39,19 @@ test.describe('Wikipedia', function() {
 
   test.it('should be able to expand and collapse menus', function(){
     this.timeout(6000);
-
+    // navigate to page
     driver.get('http://en.wikipedia.org/wiki/JavaScript');
+    // find "show" button for collapsed menu
     var element = driver.findElement({id:'collapseButton0'});
     element.getText().then(function(text){
       text.should.equal("show");
     });
-
+    // click button, text should change to hide
     element.click();
     element.getText().then(function(text){
       text.should.equal("hide");
     });
-
+    // click it again, it should revert to show
     element.click();
     element.getText().then(function(text){
       text.should.equal("show");
@@ -54,8 +60,11 @@ test.describe('Wikipedia', function() {
 
   test.it('should be able to navigate to a new page by clicking a link', function(){
     this.timeout(5000);
+    // navigate to page
     driver.get('http://en.wikipedia.org/wiki/JavaScript');
+    // find a link and click
     driver.findElement(webdriver.By.partialLinkText("jQuery")).click();  
+    // make sure clicking link navigated us to intended destination
     driver.getTitle().then(function(title){
       title.should.include("jQuery");
     });
